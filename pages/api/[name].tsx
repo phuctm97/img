f09e29a2fb0c4b2e3a999a4a120c35d2ad0420e6
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-27 14:48:43 +0700

    ♻️ Refactor server code and add docs (#3)

--- pages/api/[name].tsx
+++ pages/api/[name].tsx
@@ -1,34 +1,34 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { parseRequest } from "~server/parser";
-import { getScreenshot } from "~server/chromium";
 import { getHtml } from "~server/template";
+import { getScreenshot } from "~server/chromium";
 
-const isChromeDev = process.env.CHROME_REMOTE !== "1";
-const isHtmlDebug = process.env.HTML_DEBUG === "1";
+const isChromeLocal = process.env.CHROME_REMOTE !== "1";
+const isHTMLDebug = process.env.HTML_DEBUG === "1";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const parsedReq = parseRequest(req);
     const html = getHtml(parsedReq);
-    if (isHtmlDebug) {
+    if (isHTMLDebug) {
       res.setHeader("Content-Type", "text/html");
       res.end(html);
       return;
     }
 
     const { fileType } = parsedReq;
-    const file = await getScreenshot(html, fileType, isChromeDev);
+    const screenshot = await getScreenshot(html, fileType, isChromeLocal);
     res.statusCode = 200;
     res.setHeader("Content-Type", `image/${fileType}`);
     res.setHeader(
       "Cache-Control",
       `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
     );
-    res.end(file);
-  } catch (e) {
+    res.end(screenshot);
+  } catch (err) {
     res.statusCode = 500;
     res.setHeader("Content-Type", "text/html");
-    res.end("<h1>Internal Error</h1><p>Sorry, there was a problem</p>");
-    console.error(e);
+    res.end("<h1>Internal Error</h1><p>Sorry, there was a problem.</p>");
+    console.error(err);
   }
 };
--- server/template.ts
+++ server/template.ts
@@ -5,8 +5,8 @@ import { sanitizeHtml } from "./sanitizer";
 import { ParsedRequest } from "./types";
 import twemoji from "twemoji";
 
-const twOptions = { folder: "svg", ext: ".svg" };
-const emojify = (text: string) => twemoji.parse(text, twOptions);
+const emojify = (text: string) =>
+  twemoji.parse(text, { folder: "svg", ext: ".svg" });
 
 const fontsDir = path.join(process.cwd(), "fonts");
 const readFont = (name: string) =>
@@ -16,7 +16,7 @@ const rglr = readFont("Inter-Regular.woff2");
 const bold = readFont("Inter-Bold.woff2");
 const mono = readFont("Vera-Mono.woff2");
 
-function getCss(theme: string, fontSize: string) {
+const getCss = (theme: string, fontSize: string) => {
   let background = "white";
   let foreground = "black";
   let radial = "lightgray";
@@ -28,117 +28,110 @@ function getCss(theme: string, fontSize: string) {
   }
   return `
     @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
+      font-family: 'Inter';
+      font-style:  normal;
+      font-weight: normal;
+      src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
     }
     @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: bold;
-        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
+      font-family: 'Inter';
+      font-style:  normal;
+      font-weight: bold;
+      src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
     }
     @font-face {
-        font-family: 'Vera';
-        font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
+      font-family: 'Vera';
+      font-style: normal;
+      font-weight: normal;
+      src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
+    }
     body {
-        background: ${background};
-        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
-        background-size: 100px 100px;
-        height: 100vh;
-        display: flex;
-        text-align: center;
-        align-items: center;
-        justify-content: center;
+      background: ${background};
+      background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
+      background-size: 100px 100px;
+      height: 100vh;
+      display: flex;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
     }
     code {
-        color: #D400FF;
-        font-family: 'Vera';
-        white-space: pre-wrap;
-        letter-spacing: -5px;
+      color: #D400FF;
+      font-family: 'Vera';
+      white-space: pre-wrap;
+      letter-spacing: -5px;
     }
     code:before, code:after {
-        content: '\`';
+      content: '\`';
     }
     .logo-wrapper {
-        display: flex;
-        align-items: center;
-        align-content: center;
-        justify-content: center;
-        justify-items: center;
+      display: flex;
+      align-items: center;
+      align-content: center;
+      justify-content: center;
+      justify-items: center;
     }
     .logo {
-        margin: 0 75px;
+      margin: 0 75px;
     }
     .plus {
-        color: #BBB;
-        font-family: Times New Roman, Verdana;
-        font-size: 100px;
+      color: #BBB;
+      font-family: Times New Roman, Verdana;
+      font-size: 100px;
     }
     .spacer {
-        margin: 150px;
+      margin: 150px;
     }
     .emoji {
-        height: 1em;
-        width: 1em;
-        margin: 0 .05em 0 .1em;
-        vertical-align: -0.1em;
+      height: 1em;
+      width: 1em;
+      margin: 0 .05em 0 .1em;
+      vertical-align: -0.1em;
     }
-
     .heading {
-        font-family: 'Inter', sans-serif;
-        font-size: ${sanitizeHtml(fontSize)};
-        font-style: normal;
-        color: ${foreground};
-        line-height: 1.8;
+      font-family: 'Inter', sans-serif;
+      font-size: ${sanitizeHtml(fontSize)};
+      font-style: normal;
+      color: ${foreground};
+      line-height: 1.8;
     }`;
-}
+};
 
-export function getHtml(parsedReq: ParsedRequest) {
-  const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
+const getImage = (src: string, width = "auto", height = "225") => `<img
+  class="logo"
+  alt="Generated Image"
+  src="${sanitizeHtml(src)}"
+  width="${sanitizeHtml(width)}"
+  height="${sanitizeHtml(height)}"
+/>`;
+
+const getPlusSign = (i: number) => (i === 0 ? "" : '<div class="plus">+</div>');
+
+export const getHtml = (req: ParsedRequest) => {
+  const { text, theme, md, fontSize, images, widths, heights } = req;
   return `<!DOCTYPE html>
 <html>
-    <meta charset="utf-8">
-    <title>Generated Image</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style>
-        ${getCss(theme, fontSize)}
-    </style>
-    <body>
-        <div>
-            <div class="spacer">
-            <div class="logo-wrapper">
-                ${images
-                  .map(
-                    (img, i) =>
-                      getPlusSign(i) + getImage(img, widths[i], heights[i])
-                  )
-                  .join("")}
-            </div>
-            <div class="spacer">
-            <div class="heading">${emojify(
-              md ? marked(text) : sanitizeHtml(text)
-            )}
-            </div>
-        </div>
-    </body>
+  <meta charset="utf-8">
+  <title>Generated Image</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    ${getCss(theme, fontSize)}
+  </style>
+  <body>
+    <div>
+      <div class="spacer">
+      <div class="logo-wrapper">
+        ${images
+          .map(
+            (img, i) => getPlusSign(i) + getImage(img, widths[i], heights[i])
+          )
+          .join("")}
+      </div>
+      <div class="spacer">
+      <div class="heading">${emojify(
+        md ? marked(text) : sanitizeHtml(text)
+      )}</div>
+    </div>
+  </body>
 </html>`;
-}
-
-function getImage(src: string, width = "auto", height = "225") {
-  return `<img
-        class="logo"
-        alt="Generated Image"
-        src="${sanitizeHtml(src)}"
-        width="${sanitizeHtml(width)}"
-        height="${sanitizeHtml(height)}"
-    />`;
-}
-
-function getPlusSign(i: number) {
-  return i === 0 ? "" : '<div class="plus">+</div>';
-}
+};
