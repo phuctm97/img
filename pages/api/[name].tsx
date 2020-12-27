191da8e5b21847f0cf38d4ee4f782dc711deefdd
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-27 13:20:27 +0700

    Add chromium-based API

--- pages/api/[name].tsx
+++ pages/api/[name].tsx
@@ -1,9 +1,34 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { parseRequest } from "~server/parser";
+import { getScreenshot } from "~server/chromium";
+import { getHtml } from "~server/template";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const {
-    query: { name },
-  } = req;
+const isDev = false;
+const isHtmlDebug = process.env.HTML_DEBUG === "1";
 
-  res.status(200).json({ name });
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const parsedReq = parseRequest(req);
+    const html = getHtml(parsedReq);
+    if (isHtmlDebug) {
+      res.setHeader("Content-Type", "text/html");
+      res.end(html);
+      return;
+    }
+
+    const { fileType } = parsedReq;
+    const file = await getScreenshot(html, fileType, isDev);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", `image/${fileType}`);
+    res.setHeader(
+      "Cache-Control",
+      `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+    );
+    res.end(file);
+  } catch (e) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Internal Error</h1><p>Sorry, there was a problem</p>");
+    console.error(e);
+  }
 };
