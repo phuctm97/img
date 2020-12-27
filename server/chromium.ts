80b4affd6fbb91bc31cd1a8cd669674bda17e18f
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-27 14:07:29 +0700

    Refactor Chromium

--- server/chromium.ts
+++ server/chromium.ts
@@ -1,26 +1,45 @@
 import puppeteer, { Page } from "puppeteer-core";
-import { getOptions } from "./options";
+import ChromeAWS from "chrome-aws-lambda";
 import { FileType } from "./types";
 
-let prebuiltPage: Page | null;
+const localExePath =
+  (process.platform === "win32" &&
+    "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe") ||
+  (process.platform === "linux" && "/usr/bin/google-chrome") ||
+  "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
-async function getPage(isDev: boolean) {
-  if (prebuiltPage) return prebuiltPage;
+const getOptions = async (isLocal: boolean) =>
+  isLocal
+    ? {
+        args: [],
+        executablePath: localExePath,
+        headless: true,
+      }
+    : {
+        args: ChromeAWS.args,
+        executablePath: await ChromeAWS.executablePath,
+        headless: ChromeAWS.headless,
+      };
 
-  const options = await getOptions(isDev);
+let cachedPage: Page | null;
+
+const getPage = async (isLocal: boolean) => {
+  if (cachedPage) return cachedPage;
+
+  const options = await getOptions(isLocal);
   const browser = await puppeteer.launch(options);
-  prebuiltPage = await browser.newPage();
-  return prebuiltPage;
-}
 
-export async function getScreenshot(
+  cachedPage = await browser.newPage();
+  return cachedPage;
+};
+
+export const getScreenshot = async (
   html: string,
   type: FileType,
-  isDev: boolean
-) {
-  const page = await getPage(isDev);
+  isLocal: boolean
+) => {
+  const page = await getPage(isLocal);
   await page.setViewport({ width: 2048, height: 1170 });
   await page.setContent(html);
-  const file = await page.screenshot({ type });
-  return file;
-}
+  return page.screenshot({ type });
+};
