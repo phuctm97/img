ff793020eaa3d74c8702712c93cf6ea419e8ffa2
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-30 13:57:36 +0700

    Add comments to utils/env

--- utils/env.ts
+++ utils/env.ts
@@ -1,5 +1,14 @@
+/**
+ * Is accessing Chrome remotely?
+ */
 export const isChromeRemote = process.env.CHROME_REMOTE === "1";
 
+/**
+ * Is accessing Chrome locally?
+ */
 export const isChromeLocal = !isChromeRemote;
 
+/**
+ * Is debugging HTML output?
+ */
 export const isHTMLDebug = process.env.HTML_DEBUG === "1";
