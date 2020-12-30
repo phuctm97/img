e7a5ce24e1e52dafd4ad5b567de602e52cafd4e0
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-30 14:06:53 +0700

    Add comments to sanitizers

--- server/sanitizer.ts
+++ server/sanitizer.ts
@@ -7,5 +7,10 @@ const sanitizedTokens: { [key: string]: string } = {
   "/": "&#x2F;",
 };
 
+/**
+ * Returns an HTML from the original source that has invalid HTML tokens replaced with valid ones.
+ *
+ * @param html Input HTML
+ */
 export const sanitizeHTML = (html: string | Buffer) =>
   String(html).replace(/[&<>"'\/]/g, (key) => sanitizedTokens[key]);
